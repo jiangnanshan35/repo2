328a7305aae7dff92736d4151edf2c9ff34199a9
jiangnanshan <189745297qq.com>
2019-11-26 10:19:22 +0800

    22222

--- code8/maven_01_dao/src/main/java/com/kgc/dao/ItemsDao.java
+++
@@ -1,8 +0,0 @@
-package com.kgc.dao;
-
-import com.kgc.domain.Items;
-
-public interface ItemsDao {
-    Items findById(int id);
-    void addItems(Items items);
-}
--- code8/maven_01_service/src/main/java/com/kgc/service/ItemsService.java
+++
@@ -1,8 +0,0 @@
-package com.kgc.service;
-
-import com.kgc.domain.Items;
-
-public interface ItemsService {
-    Items findById(int id);
-    void addItem(Items items);
-}
